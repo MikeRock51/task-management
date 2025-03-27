83d6332370b2d03a9f3f6296bd60eb96f6ed5021
Mike <mikerockmusic51@gmail.com>
2025-03-27 19:47:05 +0100

    refactor tasks module: enhance validation pipe; add getTaskById method; update return types for task methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,18 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dtos/create.dto';
+import { Task } from './entities/task.entity';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService) {}
 
     @Get()
-    getAllTasks() {
+    getAllTasks(): Promise<Task[]> {
         return this.taskService.getAll();
     }
 
     @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto) {
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
         return this.taskService.createTask(createTaskDto);
+    }
+
+    @Get(':id')
+    getTaskById(@Param('id') id: number): Promise<Task> {
+        return this.taskService.getTaskById(id);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task } from './entities/task.entity';
 import { EntityManager, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,13 +12,23 @@ export class TasksService {
         readonly entityManager: EntityManager
     ) {}
 
-    getAll() {
+    getAll(): Promise<Task[]> {
         return this.taskRepository.find();
     }
 
-    async createTask(createTaskDto: CreateTaskDto) {
+    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
         const task = new Task(createTaskDto);
         return await this.entityManager.save(task);
+    }
+
+    async getTaskById(id: number): Promise<Task> {
+        const task = await this.taskRepository.findOneBy({ id });
+
+        if (!task) {
+            throw new NotFoundException(`Task with id "${id}" not found`);
+        }
+
+        return task;
     }
 
 }
