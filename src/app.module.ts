8affc63ab00d6d89fa5f428ed0e667fb703c00c7
Mike <mikerockmusic51@gmail.com>
2025-03-28 03:13:53 +0100

    feat(auth): initialize authentication module with CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot( { isGlobal: true }),
     TasksModule,
-    DatabaseModule
+    DatabaseModule,
+    AuthModule
   ],
 })
 export class AppModule {}
